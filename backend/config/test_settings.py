4c6b4ba4e92b046627875b41015571fde6caa2d9
emanM11 <emaneltabakh120@gmail.com>
2026-09-15 20:44:37 +0200

    Fix test database settings

--- backend/config/test_settings.py
+++ backend/config/test_settings.py
@@ -1,5 +1,4 @@
 from .settings import *  # noqa: F401,F403
 
-DATABASES['default']['TEST'] = {
-    'NAME': 'test_crowdfunding_opencode_db',
-}
+DATABASES['default']['TEST']['NAME'] = 'test_crowdfunding_opencode_db'
+
